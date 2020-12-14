72f2210b35062983d79ab1e3b9a04d606e0058ad
[stefanhallberg123]”git config --global user.name [stefanhallberg123]” <[stefanhalllberg@live.se]>
2020-12-14 12:57:55 +0100

    components and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/admin/Login/Login";
+import StartCars from "./components/admin/Start_cars/StartCars";
+import StartMessages from "./components/admin/Start_messages/StartMessages";
+import AddCar from "./components/admin/Add_car/AddCar";
+import AddMessage from "./components/admin/Add_message/AddMessage";
+import EditCar from "./components/admin/Edit_car/EditCar";
+import About from "./components/customer/About/About";
+import Home from "./components/customer/Home/Home";
+import Car from "./components/customer/Car/Car";
+import NotFound from "./components/admin/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        {/* admin */}
+        <Route path="/admin/login">
+          <Login />
+        </Route>
+        <Route path="/admin/start/cars">
+          <StartCars />
+        </Route>
+        <Route path="/admin/start/messages">
+          <StartMessages />
+        </Route>
+        <Route path="/admin/car/add">
+          <AddCar />
+        </Route>
+        <Route path="/admin/cars/edit/:id">
+          <EditCar />
+        </Route>
+        <Route path="/admin/messages/add">
+          <AddMessage />
+        </Route>
+        <Route path="/admin/messages/add/:id">
+          <AddMessage />
+        </Route>
+        {/* customer */}
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/:id">
+          <Car />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/admin/Edit_car/EditCar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EditCar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
